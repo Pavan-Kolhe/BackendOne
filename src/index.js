5fda47a530c6ca72708ddebd47ca1b7201e6a655
Pavan-Kolhe <pavandigambarkolhe@gmail.com>
2025-07-06 16:36:06 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
  //require("dotenv").config({path : "./env"})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({
     path:"./env"
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+
+    app.on("error", (error) => {
+      console.error("ERROR :", error);
+      throw error;
+    });
+
+    const port = process.env.PORT ||8000;
+
+    app.listen(port,()=>{
+        console.log(`server is running at port 
+            : ${port}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed!!!",err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+// bar bar ek hi wrapper type duction hai muze do me execute karunga
+
+const asyncHandler =(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((error)=>next(error))
+    }
+}
+
+export {asyncHandler}
+
+// const asyncHandler = () =>{}
+// const asyncHandler = (func) =>{}
+// const asyncHandler = (func) => async()=>{}
+
+//asyn await mathod
+// const asyncHandler =(fn)=> (req,res,next)=>{  
+//     try {
+//         await fn(req,res,next);
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message : error.message;
+//         })
+//     }
+// }
