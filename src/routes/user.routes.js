c963e6639573e9e811c9644dd672af2cd3eb979f
Pavan-Kolhe <pavandigambarkolhe@gmail.com>
2025-07-09 15:44:57 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
-  changeCurrentPassword
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
 } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,10 +35,18 @@ router.route("/login").post(loginUser);
 
 //secure routes
 router.route("/logout").post(verifyJWT,logoutUser)   // added verifyJWT as middleware jo ki verify karne k baad req.user add karega
-
-
 router.route("/refresh-token").post(refreshAccessToken)
-
 router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage);
+
+//params {username}
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+
+router.route("/history").get(verifyJWT,getWatchHistory)
+
 
 export default router;
